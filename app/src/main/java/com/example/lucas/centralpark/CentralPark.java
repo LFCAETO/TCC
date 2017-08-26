f5b9ff820396d3a9806f0f6a7fb19659752b8d9a
lucaslsbr.ibm.com <Lucas@oc2316646280.ibm.com>
2017-08-26 15:43:20 -0300

    Initial commit

---
+++ app/src/main/java/com/example/lucas/centralpark/CentralPark.java
@@ -0,0 +1,102 @@
+package com.example.lucas.centralpark;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CentralPark extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_central_park);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.conteiner, new HomeFragment()).commit();
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.central_park, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if (id == R.id.nav_home) {
+            fragmentManager.beginTransaction().replace(R.id.conteiner, new HomeFragment()).commit();
+        } else if (id == R.id.nav_informe) {
+            fragmentManager.beginTransaction().replace(R.id.conteiner, new InformeFragment()).commit();
+        }
+   //     } else if (id == R.id.nav_slideshow) {
+    //        fragmentManager.beginTransaction().replace(R.id.conteiner, new HomeFragment()).commit();
+    //    } else if (id == R.id.nav_manage) {
+    //        fragmentManager.beginTransaction().replace(R.id.conteiner, new HomeFragment()).commit();
+   //     } else if (id == R.id.nav_share) {
+   //         fragmentManager.beginTransaction().replace(R.id.conteiner, new HomeFragment()).commit();
+   //     } else if (id == R.id.nav_send) {
+   //         fragmentManager.beginTransaction().replace(R.id.conteiner, new HomeFragment()).commit();
+   //     }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
